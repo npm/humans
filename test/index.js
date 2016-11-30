5db93a717087ce51ef0fa56eb1f228b05ec93917
Laurie Voss <github@seldo.com>
2016-11-30 13:47:00 -0800

    Jerry and Andrea

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ var test = require("tap").test
 
 test("humans", function (t) {
   t.ok((typeof humans === "object"), "humans are objects")
-  t.equal(Object.keys(humans).length, 21, "there are 21 humans")
+  t.equal(Object.keys(humans).length, 23, "there are 23 humans")
   t.ok(humans.ceejbot, "one of them is ceejbot")
   t.equal(humans.ceejbot.email, "ceej@npmjs.com", "ceejbot's email address is ceej@npmjs.com")
   t.equal(humans.ceejbot.avatar, "https://s.gravatar.com/avatar/d4d41847af095b91aa8ed1d1145f5e03?size=512&default=retro", "ceejbot has an HTTPS gravatar")
